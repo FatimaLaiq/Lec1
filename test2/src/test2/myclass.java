d0d49f3aef377ef9fca5e1361b8de1517735e239
vbstudent <vbstudent@CL3-09.uit.edu>
2018-11-17 13:11:17 +0500

    rterterther

--- test2/src/test2/myclass.java
+++ test2/src/test2/myclass.java
@@ -4,14 +4,23 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.chrome.ChromeOptions;
+         
 public class myclass {
 
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\vbstudent\\Desktop\\testcase\\chromedriver.exe");
+	ChromeOptions options=new ChromeOptions();
+	options.addArguments("start-maximixed");
+	options.addArguments("disable-infobar ");
+	options.addArguments("--incognito");
+	options.addArguments("--ignore-certificate-error");
+	options.addArguments("--disable-popup-blocking");
+		
+		
 		WebDriver driver = new ChromeDriver();
 		
-		//Opening the Website		
+		//Opening the Website		                                
 			driver.get("https://demo.opencart.com/index.php?route=account/login");	
 			driver.manage().window().maximize();
 			 System.out.println("Window is opened");
@@ -66,17 +75,38 @@ public class myclass {
 			searchButton.click(); 
 			System.out.println("search button is clicked!");
 			
-			//Clicking on Cart button
-			WebElement cartButton;  
-			cartButton = driver.findElement(By.xpath("//*[@id=\"content\"]/div[3]/div/div/div[2]/div[2]/button[1]"));
-			cartButton.click(); 
+			//Cart button
+			WebElement tButton;  
+		tButton = driver.findElement(By.xpath("//*[@id=\"content\"]/div[3]/div/div/div[2]/div[1]/h4/a"));
+			tButton.click(); 
 			System.out.println("Cart button is clicked!");
 			
+			
 			//Clicking on Cart button
-			WebElement hhButton;  
-			hhButton = driver.findElement(By.className("btn btn-primary btn-lg btn-block"));
-			hhButton.click(); 
-			System.out.println("hh button is clicked!");
+			WebElement cartButton;  
+			cartButton = driver.findElement(By.xpath("//*[@id=\"button-cart\"]"));
+			cartButton.click(); 
+			System.out.println("Cart is added!");
+			
+			//Clicking on Cart button
+			WebElement ppButton;  
+			ppButton = driver.findElement(By.xpath("//*[@id=\"cart\"]/button"));
+			ppButton.click(); 
+			System.out.println("Cart menu!");
+			
+			//Clicking on Cart button
+			WebElement pppButton;  
+			pppButton = driver.findElement(By.xpath("//*[@id=\"cart\"]/ul/li[2]/div/p/a[1]/strong"));
+			pppButton.click(); 
+			System.out.println("view!");
+			
+			//Clicking on Cart button
+			WebElement poButton;  
+			poButton = driver.findElement(By.xpath("//*[@id=\"content\"]/form/div/table/tbody/tr/td[4]/div/span/button[2]"));
+			poButton.click(); 
+			System.out.println("Deleted");
+			
+			
 	
 	}
 
